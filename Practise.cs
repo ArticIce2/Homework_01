4f52cedb3d632e291cb2f244f7b61d2de1edfbe3
Artic <artic.blue17@mail.ru>
2023-06-13 00:53:09 +0300

    Дз - 1

---
+++ Practise.cs
@@ -0,0 +1,78 @@
+
+// // //Задача №2
+// // System.Console.Write("Введите первое число -> ");
+// // int a = Convert.ToInt32(Console.ReadLine());
+// // System.Console.Write("Введите второе число -> ");
+// // int b = Convert.ToInt32(Console.ReadLine());
+// // System.Console.Write("Введите максимальное число ->");
+// // int max = Convert.ToInt32(Console.ReadLine());
+// // if(a > max)
+// {
+// System.Console.Write($" число {a} больше {max}");
+// }
+// else
+// {
+// System.Console.Write($" число {a} меньше {max}");
+// }
+// if(b > max)
+// {
+// System.Console.Write($" число {b} больше {max}");
+// }
+// else
+// {
+// System.Console.Write($" число {b} меньше {max}");
+// }
+
+// //Задача №4
+// System.Console.Write("Введите первое число -> ");
+// int t = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int y= Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите третье число -> ");
+// int u = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+
+// if(t > max)
+// {
+// max = t;
+// }
+
+// if(y > max)
+// {
+// max = y;
+// }
+
+// if(u > max)
+// {
+// max = u;
+// }
+
+// System.Console.WriteLine($"Максимальное число -> {max}");
+
+
+//Задача 6
+// System.Console.Write("Введите число которое вы будете проверять -> ");
+// int g = Convert.ToInt32(Console.ReadLine());
+
+// if(g % 2 == 0)
+// {
+// System.Console.WriteLine($" Данное число чётное");
+// }
+// else
+// System.Console.WriteLine($" Данное число нечётное");
+
+// //Задача 8
+// System.Console.Write("Введите число N -> ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+
+// if(number > 1)
+// {
+//     while(evenNumber <= number)
+//     {
+//     System.Console.WriteLine(evenNumber + "");
+//     evenNumber = evenNumber + 2;
+//     }
+// }
